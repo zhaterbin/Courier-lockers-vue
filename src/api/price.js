d3d49c1dea1ce6c6df036b65036eff8412525e67
超 张 <2074119880@qq.com>
2024-05-09 13:16:52 +0800

    first commit

---
+++ src/api/price.js
@@ -0,0 +1,36 @@
+import request from '@/utils/request'
+
+
+export function  fetchReportList(data) {
+        return request({
+          url: `/api/InStorage/PriceRulerPage`,
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data
+        })
+    }
+
+export function  InPriceRuler(data) {
+      return request({
+        url: `/api/InStorage/InPriceRuler`,
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data
+      })
+  }
+
+  export function  DeleteRuler(data) {
+    return request({
+      url: `/api/InStorage/DeleteRuler`,
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data
+    })
+}
+
